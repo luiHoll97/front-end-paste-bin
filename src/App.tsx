f8361fc68c0c9fed88a95d9c9c1b3f624a13a7c6
leah <theloadingpage@gmail.com>
2022-09-07 17:21:54 +0100

    format and lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import ListAllPastes from "./components/ListAllPastes";
 
 function App(): JSX.Element {
-  return <>
-  <h1>PASTEBIN TEST </h1>
-  <ListAllPastes/>
-  </>
-
+  return (
+    <>
+      <h1>PASTEBIN TEST </h1>
+      <ListAllPastes />
+    </>
+  );
 }
 
 export default App;
--- src/components/ListAllPastes.tsx
+++ src/components/ListAllPastes.tsx
@@ -1,44 +1,38 @@
 import { useEffect, useState } from "react";
 
 interface IPaste {
-    paste_id: number,
-    snippet: string,
-    owner: string
+  paste_id: number;
+  snippet: string;
+  owner: string;
 }
 
+function ListAllPastes(): JSX.Element {
+  const [allPastes, setAllPastes] = useState<IPaste[]>([]);
 
-function ListAllPastes():JSX.Element {
+  useEffect(() => {
+    getAllPastes();
+  }, []);
 
-   const [allPastes, setAllPastes] = useState<IPaste[]>([]);
-   
-   useEffect(() => {
-    getAllPastes()
-    }, []
-   )
-    
-
-   async function getAllPastes() {
+  async function getAllPastes() {
     try {
-        const response = await fetch("https://a2-paste-bin.herokuapp.com/pastes");
-        const pasteJSON = await response.json()
-        setAllPastes(pasteJSON);
+      const response = await fetch("https://a2-paste-bin.herokuapp.com/pastes");
+      const pasteJSON = await response.json();
+      setAllPastes(pasteJSON);
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-    }
+  }
 
-    const allPastesList = allPastes.map((paste: IPaste) => {
-        return (<ul key={paste.paste_id}>
-            <li>{paste.snippet}</li>
-            <li>{paste.owner}</li>
-        </ul>
-        )
-    } )
-    
-    return <>
-    {allPastesList}
-    </>
+  const allPastesList = allPastes.map((paste: IPaste) => {
+    return (
+      <ul key={paste.paste_id}>
+        <li>{paste.snippet}</li>
+        <li>{paste.owner}</li>
+      </ul>
+    );
+  });
 
+  return <>{allPastesList}</>;
 }
 
 export default ListAllPastes;
